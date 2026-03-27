a4d9f0d7b8840507829e1c8642e7f161ed274653
Ashwini Jaiswal. <ashwinisj2005@gmail.com>
2026-03-27 17:12:09 +0530

    Update script.js

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -1,7 +1,7 @@
 const counters = document.querySelectorAll('.counter')
 
 counters.forEach(counter => {
-    counter.innerText = ''
+    counter.innerText = '0'
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
